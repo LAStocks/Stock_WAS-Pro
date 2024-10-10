6bb57bbe1454ccaf36f41f5907fd25e48ec8f68e
JinSeokAn <dkswlstjr000@naver.com>
2024-10-10 11:22:10 +0900

    feat(auth): implement user registration with MyBatis and Swagger

--- src/main/java/stock/controller/AuthController.java
+++ src/main/java/stock/controller/AuthController.java
@@ -1,5 +1,7 @@
 package stock.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
+@Api(tags = "Authentication Controller", description = "사용자 인증과 관련된 작업들")
 public class AuthController {
 
     //@Autowired
@@ -22,7 +25,7 @@ public class AuthController {
     private UserInfoMapper userInfoMapper;
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     // 로그인 후 토큰 발급
     @PostMapping("/login")
@@ -37,11 +40,12 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
+    @ApiOperation(value = "User Sign Up", notes = "제공된 정보를 사용하여 새로운 사용자를 등록합니다.")
     public ResponseEntity<String> signUp(@RequestBody UserInfo userInfo) {
         if(userInfoMapper.findByUsername(userInfo.getUsername()) != null) {
             return new ResponseEntity<>("회원가입 하려 한 ID가 존재합니다.", HttpStatus.BAD_REQUEST);
         }
-        userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+        userInfo.setPassword(bCryptPasswordEncoder.encode(userInfo.getPassword()));
         userInfoMapper.save(userInfo);
 
         return new ResponseEntity<>("회원가입이 완료되었습니다.", HttpStatus.OK);
